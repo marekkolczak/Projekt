866e48b6b3fcaabc640dda2d73a1b6095beec551
marekkolczak <marekkolczak4@yahoo.co.uk>
2013-11-27 01:20:52 +0100

    ostatni kommit

--- Kalkulator/Kalkulator/Calc.cs
+++ Kalkulator/Kalkulator/Calc.cs
@@ -8,6 +8,24 @@ namespace Kalkulator
 {
     class Calc
     {
-        
+        static double a;
+        static double b;
+        public static double Add(int a, int b)
+        {
+            return (a + b);
+        }
+        public static double Multiplication(int a, int b)
+        {
+            int kk;
+            return (a * b);
+        }
+        public static double Substraction(int a, int b)
+        {
+            return (a - b);
+        }
+        public static float Devide(float a, float b)
+        {
+            return (a / b);
+        }
     }
 }
--- Kalkulator/Kalkulator/Program.cs
+++ Kalkulator/Kalkulator/Program.cs
@@ -8,8 +8,67 @@ namespace Kalkulator
 {
     class Program
     {
+
+
+
+        public static void MainWindow()
+        {
+            Console.Clear();
+            Console.WriteLine("To jest kalkulator");
+            Console.WriteLine("[1] mnozenie");
+            Console.WriteLine("[2] Dzielenie");
+            Console.WriteLine("[3] Dodawanie");
+            Console.WriteLine("[4] Odejmowanie");
+            string wybor = Console.ReadLine();
+            int first, second;
+
+            switch (wybor)
+            {
+                case "1":
+                    {
+                        LoadData(out first, out second);
+                        Console.WriteLine(Calc.Multiplication(first, second));
+                    } break;
+
+                case "2":
+                    {
+                        LoadData(out first, out second);
+                        Console.WriteLine("{0:F}", Calc.Devide(first, second));
+                    } break;
+
+                case "3":
+                    {
+                        LoadData(out first, out second);
+                        Console.WriteLine(Calc.Add(first, second));
+                    }; break;
+
+                case "4":
+                    {
+                        LoadData(out first, out second);
+                        Console.WriteLine(Calc.Substraction(first, second));
+                    } break;
+                default: break;
+            }
+
+        }
+
+        static void LoadData(out int a, out int b)
+        {
+            Console.WriteLine("Podaj a");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj b");
+            b = int.Parse(Console.ReadLine());
+
+        }
         static void Main(string[] args)
         {
+            MainWindow();
+            Console.ReadKey();
+
+
         }
     }
+        
+    
+    
 }
